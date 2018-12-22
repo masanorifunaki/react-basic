311a91c793169d4f042c4aa16b32ec0665eae7bd
舟木正憲 <funakimfunaki@gmail.com>
2018-12-22 16:14:12 +0900

    add

---
+++ src/ClassComponent1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class ClassComponent1 extends React.Component {
+  render() {
+    return <div>1</div>;
+  }
+}
---
+++ src/ClassComponent3.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class ClassComponent3 extends React.Component {
+  render() {
+    return <div>3</div>;
+  }
+}
